52c04cb156c79ded7a9f9913c448490bfa12f78d
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2024-02-12 18:16:55 +0530

    linked contact us page to navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
           // },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://topmate.io/sauraav',
+            href: 'https://www.datachamp.in/contact',
             label: 'Contact us',
             position: 'right',
           },
